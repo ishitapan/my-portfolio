72b9180983cb3b82fa08b103bafb5bdc960d8935
ishitapan <panishita21@gmail.com>
2026-01-18 13:18:29 +0530

    Initial React portfolio

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <div>
+      <h1>Hello, I’m Ishita 👋</h1>
+      <p>Java Backend Developer | React Learner</p>
+    </div>
+  )
+}
+
+export default App
+
+
